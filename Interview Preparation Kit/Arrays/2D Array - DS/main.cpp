79b2f71470375056b32bad977b94bd97c7541547
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-02-26 14:21:06 +0900

    2D Array - DS

--- Interview Preparation Kit/Arrays/2D Array - DS/main.cpp
+++ Interview Preparation Kit/Arrays/2D Array - DS/main.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
-// Complete the hourglassSum function below.
-int hourglassSum(int arr[6][6]) {
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int arr[6][6];
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      cin >> arr[i][j];
+    }
+  }
   int max = -63;
   for (int r = 0; r < 4; r++) {
     for (int c = 0; c < 4; c++) {
@@ -15,19 +22,6 @@ int hourglassSum(int arr[6][6]) {
       if (sum > max) max = sum;
     }
   }
-  return max;
-}
-
-int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int arr[6][6];
-  for (int i = 0; i < 6; i++) {
-    for (int j = 0; j < 6; j++) {
-      cin >> arr[i][j];
-    }
-  }
-  int result = hourglassSum(arr);
-  cout << result << "\n";
+  cout << max << "\n";
   return 0;
 }
