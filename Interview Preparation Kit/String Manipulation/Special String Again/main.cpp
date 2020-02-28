8f480c6c83136e33da68925379994d85e439842d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-02-28 17:50:03 +0900

    Special String Again

--- Interview Preparation Kit/String Manipulation/Special String Again/main.cpp
+++ Interview Preparation Kit/String Manipulation/Special String Again/main.cpp
@@ -9,6 +9,8 @@ long substrCount(int n, string s) {
   long cnt = 0;
   char cur = ' ';
   // 1st pass
+  // 1. Build a list of tuples such that the string "aaabbc" can be squashed
+  // down to [("a", 3), ("b", 2), ("c", 1)].
   for (int i = 0; i < n; i++) {
     if (s[i] == cur)
       cnt++;
@@ -20,9 +22,13 @@ long substrCount(int n, string s) {
   }
   v.push_back({cur, cnt});
   // 2nd pass
+  // 2. add to answer all combinations of substrings from these tuples which
+  // would represent palindromes which have all same letters.
   long ans = 0;
   for (pair<char, long> i : v) ans += (i.second * (i.second + 1)) / 2;
   // 3rd pass
+  // 3. traverse this list to specifically find the second case mentioned in
+  // probelm.
   for (long i = 1; i < (long)v.size() - 1; i++) {
     if (v[i - 1].first == v[i + 1].first && v[i].second == 1)
       ans += min(v[i - 1].second, v[i + 1].second);
