d596dfb81f1e44555ade09ca0869b67ed3ddc67c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-03-02 21:12:49 +0900

    Fraudulent Activity Notifications

---
+++ Interview Preparation Kit/Sorting/Fraudulent Activity Notifications/README.md
@@ -0,0 +1,5 @@
+# Fraudulent Activity Notifications
+
+## UNSOLVED
+
+[https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem](https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem)
---
+++ Interview Preparation Kit/Sorting/Fraudulent Activity Notifications/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int expenditure[200001];
+
+int main() {
+  // 1.) Counting sort
+  // 2.) A Queue
+  // 3.) Pay attention to the even case.
+  // 4.) Integer division is a blessing and a curse, be careful.
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  freopen("input05.txt", "r", stdin);
+  int n, d;
+  cin >> n >> d;
+  return 0;
+}
